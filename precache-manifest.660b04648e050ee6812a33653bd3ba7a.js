a9fb980806a37702c24adeb8d63d04486c39b140
CyberPatrick <57152405+CyberPatrick@users.noreply.github.com>
2020-08-23 20:49:33 +0300

    Updates

---
+++ precache-manifest.660b04648e050ee6812a33653bd3ba7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4466ae8a805acba4f9cb0b04734f8a8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c9a3649579b10a5535bd",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "107e362741c2cf5743f1",
+    "url": "./static/css/main.5fccc0af.chunk.css"
+  },
+  {
+    "revision": "c9a3649579b10a5535bd",
+    "url": "./static/js/2.034ad65f.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.034ad65f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "107e362741c2cf5743f1",
+    "url": "./static/js/main.8a3b0805.chunk.js"
+  },
+  {
+    "revision": "556dfe5fcd8d7c77ee4f",
+    "url": "./static/js/runtime-main.c21f4b73.js"
+  }
+]);
